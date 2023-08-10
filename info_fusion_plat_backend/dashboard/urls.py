ee1664bb68e904ac007a80bf84ce302bb33eda36
kalinote <840746219@qq.com>
2023-08-10 12:54:26 +0800

    获取24小时高权重信息接口

--- info_fusion_plat_backend/dashboard/urls.py
+++ info_fusion_plat_backend/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CollectedInfoSummaryData
+from .views import CollectedInfoSummaryData, DailyHighWeightInfo
 
 urlpatterns = [
     path('api/v1/dashboard/collected_info_summary_data', CollectedInfoSummaryData.as_view(), name='collected_info_summary_data'),
+    path('api/v1/dashboard/daily_high_weight_info', DailyHighWeightInfo.as_view(), name='daily_high_weight_info'),
 ]
