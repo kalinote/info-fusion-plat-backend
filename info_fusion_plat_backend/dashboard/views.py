b7379faa5c1f17265b53ad606ef0c9c42389a508
kalinote <840746219@qq.com>
2023-08-13 04:09:33 +0800

    更改采集节点信息接口，从crawlab获取信息

--- info_fusion_plat_backend/dashboard/views.py
+++ info_fusion_plat_backend/dashboard/views.py
@@ -1,7 +1,7 @@
+import json
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
-
-import time
+import requests
 
 class CollectedInfoSummaryData(APIView):
     def get(self, request, *args, **kwargs):
@@ -85,6 +85,17 @@ class DailyHighWeightInfo(APIView):
 
 class NodeInfo(APIView):
     def get(self, request, *args, **kwargs):
+
+        # TODO: 后面用数据库管理cookie，并封装成接口
+        login_response = requests.get(
+            url="http://backend-service:8080/api/stats/overview",
+            headers={
+                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YzhjYzliZjI3MTg1ZmI1ZjM0NTkxOSIsIm5iZiI6MTY5MDk5NzA2MCwidXNlcm5hbWUiOiJhZG1pbiJ9.KSgi1ZhzaC7gh_xslot05dMtBR6XK152diO5M69C32M'
+            }
+        )
+
+        node_info = json.loads(login_response.text)
+
         response_data = {
             'code': 0,
             'data': {"list": [
@@ -93,25 +104,25 @@ class NodeInfo(APIView):
                     'title': "在线节点",
                     'color': { 'background': "#7ABBFF" },
                     'icon': "node",
-                    'value': 1
+                    'value': node_info['data'].get('nodes', 0)
                     },
                     {
                     'title': "项目",
                     'color': { 'background': "#7ABBFF" },
                     'icon': "project",
-                    'value': 2
+                    'value': node_info['data'].get('projects', 0)
                     },
                     {
                     'title': "爬虫",
                     'color': { 'background': "#7ABBFF" },
                     'icon': "spider",
-                    'value': 0
+                    'value': node_info['data'].get('spiders', 0)
                     },
                     {
                     'title': "定时任务",
                     'color': { 'background': "#7ABBFF" },
                     'icon': "timed-task",
-                    'value': 0
+                    'value': node_info['data'].get('schedules', 0)
                     }
                 ],
                 [
@@ -119,11 +130,11 @@ class NodeInfo(APIView):
                     'title': "任务总数",
                     'color': { 'background': "#7ABBFF" },
                     'icon': "task-count",
-                    'value': 0
+                    'value': node_info['data'].get('tasks', 0)
                     },
                     {
                     'title': "错误任务",
-                    'value': 0,
+                    'value': node_info['data'].get('error_tasks', 0),
                     'color': { 'background': "#F87F7D" },
                     'icon': "error"
                     },
@@ -131,7 +142,7 @@ class NodeInfo(APIView):
                     'title': "结果总数",
                     'color': { 'background': "#B3E19D" },
                     'icon': "result-count",
-                    'value': 3245200
+                    'value': node_info['data'].get('results', 0)
                     },
                     {
                     'title': "正在运行",
