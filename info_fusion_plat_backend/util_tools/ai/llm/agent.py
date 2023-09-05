3919a75943d11b14664ed9ccf5ebbe1d25373241
kalinote <840746219@qq.com>
2023-09-05 10:52:02 +0800

    增加agnet身份

--- info_fusion_plat_backend/util_tools/ai/llm/agent.py
+++ info_fusion_plat_backend/util_tools/ai/llm/agent.py
@@ -26,24 +26,27 @@ class AgentManager:
             self,
             default_thinking_type=Thinking.types.DEFAULT
         ):
-        self.global_main_agent = Agent(
+
+        # 全局主agent，所有agent都是这个agent的子agent
+        self.global_main_agent = ManagerAgent(
             global_main_agent=True,
-            thinking_type=default_thinking_type
+            thinking_type=default_thinking_type,
+            one_time=False,
+            agent_role='manager'
         )
 
 
 class Agent:
-    init_prompt = ""
+    init_prompt = None
 
     def __init__(
             self,
             global_main_agent=False,
             parent_agent=None,
-            thinking_type=None, 
-            memory={
-                "memory": []
-            },
-            one_time=False
+            thinking_type=Thinking.types.DEFAULT, 
+            memory=None,
+            one_time=False,
+            agent_role=None
         ):
 
         # 是否为全局主agent
@@ -59,7 +62,7 @@ class Agent:
         self.thinking = Thinking(thinking_type=thinking_type)
 
         # agent的回忆，由多轮对话产生，也可以在初始化时手动构造
-        self.memory = memory
+        self.memory = memory if memory else {"memory": []}
 
         # 是否一次性使用，设置为true时，在进行一次thinking后立即销毁,只能在作为子agent时设置为true
         self.one_time = one_time if parent_agent else False
@@ -72,3 +75,66 @@ class Agent:
 
         # agnet创建时间
         self.create_time = datetime.now().strftime("%Y:%m:%d %H:%M:%S")
+
+        # agent的角色，分为manager/worker/planner, 默认为worker
+        self.agent_role= agent_role
+
+        # agent的状态
+        self.status = 'wait for init'
+
+
+class ManagerAgent(Agent):
+    def __init__(
+            self, 
+            global_main_agent=False, 
+            parent_agent=None, 
+            thinking_type=Thinking.types.DEFAULT, 
+            memory=None, 
+            one_time=False, 
+            agent_role='manager'
+        ):
+        super().__init__(
+            global_main_agent, 
+            parent_agent, 
+            thinking_type, 
+            memory, 
+            one_time, 
+            agent_role
+        )
+
+class WorkerAgent(Agent):
+    def __init__(
+            self, 
+            parent_agent=None, 
+            thinking_type=Thinking.types.DEFAULT, 
+            memory=None, 
+            one_time=False, 
+            agent_role='worker'
+        ):
+        super().__init__(
+            parent_agent, 
+            thinking_type, 
+            memory, 
+            one_time, 
+            agent_role
+        )
+
+class PlannerAgent(Agent):
+    def __init__(
+            self, 
+            parent_agent=None, 
+            thinking_type=Thinking.types.DEFAULT, 
+            memory=None, 
+            one_time=False, 
+            agent_role='planner'
+        ):
+        super().__init__(
+            parent_agent, 
+            thinking_type, 
+            memory, 
+            one_time, 
+            agent_role
+        )
+
+
+agent_manager = AgentManager()
